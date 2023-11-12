39fa6bafaa5b283778e1cf02f70628817e7ab1c2
Guiherme Hassã <gui.hassa@hotmail.com>
2023-11-12 15:44:00 -0300

    🚀 Start Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './assets/styles/App.scss';
+
+function App() {
+  return (
+    <div className="SSBook">
+      <div className='Appteste'>
+        ola
+      </div>
+    </div>
+  );
+}
+
+export default App;
