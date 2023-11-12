9a082ccfb86e207d994cbb0fcb99acf4eb8ffe20
Guiherme Hassã <gui.hassa@hotmail.com>
2023-11-12 16:34:23 -0300

    🚀 Insert bootstrap on project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './assets/styles/App.scss';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="SSBook">
-      <div className='Appteste'>
-        ola
-      </div>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+export default function Header(){
+  return(
+    <header className="header">
+      <Container>
+        Header
+      </Container>
+    </header>
+  )
+}
